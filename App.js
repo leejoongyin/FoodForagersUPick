8209851defa65f82cb22f896f87dd5fea9deffef
Jessica Nguyen <jdn039@ucsd.edu>
2020-11-21 15:30:55 -0600

    preferences

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PreferencesScreen from './src/preferences';
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Preferences"
+        onPress={() => navigation.navigate('Preferences')}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Preferences" component={PreferencesScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
