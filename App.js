a3b2a6f634ec8f9f7e4f194c19953ac0e0ea06bf
Jessica Nguyen <jdn039@ucsd.edu>
2020-11-25 14:20:02 -0600

    ui done

--- App.js
+++ App.js
@@ -1,34 +1,56 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React,  {Component} from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import PreferencesScreen from './src/preferences';
+import Preferences from './src/preferences';
+import Home from './src/home';
 
 const Stack = createStackNavigator();
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Preferences"
-        onPress={() => navigation.navigate('Preferences')}
-      />
-    </View>
-  );
+const LightTheme = {
+  dark: false,
+  colors: {
+    primary: '',
+    background: '#F2E9E0',
+    card: '',
+    text: '#6B222D',
+    border: '',
+    notficiation: ''
+  }
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Preferences" component={PreferencesScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  );
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isDarkmode: true 
+    };
+    var mode = (this.getIsDarkmode() ? styles.darkmode : styles.lightmode);
+    console.log(mode);
+    
+  }
+
+  toggleDarkmode() {
+    this.setState({ isDarkmode: !this.state.isDarkmode});  
+  }
+
+  getIsDarkmode() {
+    return this.state.isDarkmode;
+  }
+
+  render() {
+    return (
+      <NavigationContainer theme={LightTheme}>{
+        <Stack.Navigator initialRouteName="Home" options={{isDarkmode: this.getIsDarkmode()}} screenOptions={{headerStyle: {backgroundColor: '#E2D6C8'}, headerTintColor: '#442C1E', headerTitleStyle: {textAlign: 'center', marginLeft: -30}}}>
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}} initialParams={{ isDarkmode: this.getIsDarkmode(), toggleDarkmode: this.toggleDarkmode.bind(this), getIsDarkmode: this.getIsDarkmode.bind(this) }}/> 
+          <Stack.Screen name="Preferences" component={Preferences} options={{title: 'Preferences'}} initialParams={{ isDarkmode: this.getIsDarkmode(), toggleDarkmode: this.toggleDarkmode.bind(this), getIsDarkmode: this.getIsDarkmode.bind(this) }}/> 
+        </Stack.Navigator>
+      }</NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/preferences.js
+++ src/preferences.js
@@ -1,67 +1,181 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
-// import { SelectMultipleButton, SelectMultipleGroupButton } from "react-native-selectmultiple-button";
+import React, {Component} from 'react';
+import { Text, View, TextInput, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import SelectionGroup, { SelectionHandler } from 'react-native-selection-group';
 
-function PreferencesScreen({ navigation }) {
-  const [value, onChangeText] = React.useState('');
-  // const multipleGroupData = [
-  //   { value: "$" },
-  //   { value: "$$" },
-  //   { value: "$$$" }
-  // ];
-  // const ios_blue = "#007AFF";
-  // this.state = {
-  //   multipleSelectedData: [],
-  //   multipleSelectedData_group: selectedValues1,
-  //   multipleSelectedData_group_limited: []
-  // };
+const MAX_SELECT = 10;
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Input Preferences</Text>
-      <Text>Where would you like to eat?*</Text>
-      <TextInput
-        placeholder="Enter zipcode"
-        onChangeText={text => onChangeText(text)}
-        value={value}
-      />
-      <Text>When would you like to eat?*</Text>
-      <TextInput
-        placeholder="Enter time"
-        onChangeText={text => onChangeText(text)}
-        value={value}
-      />
-      <Text>Budget</Text>
-      {/* <SelectMultipleGroupButton
-        containerViewStyle={{
-          justifyContent: "flex-start"
-        }}
-        highLightStyle={{
-          borderColor: "gray",
+export default class Preferences extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.budgetSelectionHandler = new SelectionHandler({ maxMultiSelect: MAX_SELECT, allowDeselect: true, defaultSelection: null });
+    this.dietSelectionHandler = new SelectionHandler({ maxMultiSelect: MAX_SELECT, allowDeselect: true, defaultSelection: null });
+    this.cuisineSelectionHandler = new SelectionHandler({ maxMultiSelect: MAX_SELECT, allowDeselect: true, defaultSelection: null });
+    this.restaurantSelectionHandler = new SelectionHandler({ maxMultiSelect: MAX_SELECT, allowDeselect: true, defaultSelection: null });
+    this.state = {
+        selectedItems: null
+    };
+  }
 
-          backgroundColor: "transparent",
+  onItemSelected = (item, allSelectedItems) => {
+    this.setState({ selectedItems: allSelectedItems });
+  }
 
-          textColor: "gray",
+  renderButton = (data, index, isSelected, onPress) => {
+    return (
+      <TouchableOpacity
+        onPress={onPress}
+        key={index}
+        style={[styles.button,
+        { backgroundColor: isSelected ? '#A6433F' : '#FFF'}]}>
+        <Text style={[styles.buttonText, {color: isSelected ? '#FFF' : '#000'}]}>{data.optionText}</Text>
+      </TouchableOpacity>
+    );
+  }
 
-          borderTintColor: ios_blue,
+  render() {
+    const budgetData = [
+      { id: 1, optionText: '$' },
+      { id: 2, optionText: '$$' },
+      { id: 3, optionText: '$$$' }
+    ];
 
-          backgroundTintColor: "transparent",
+    const dietaryRequirements = [
+      { id: 4, optionText: 'Lactose intolerant' },
+      { id: 5, optionText: 'Nut allergy' },
+      { id: 6, optionText: 'Shellfish allergy' },
+      { id: 7, optionText: 'Vegan' },
+      { id: 8, optionText: 'Vegetarian' },
+      { id: 9, optionText: 'Kosher' }, 
+      { id: 10, optionText: 'Paleo' }
+    ];
 
-          textTintColor: ios_blue
-        }}
-        onSelectedValuesChange={selectedValues =>
-          this._groupButtonOnSelectedValuesChange(selectedValues)
-        }
-        group={multipleGroupData}
-      />; */}
-      <Text>Dietary Requirements</Text>
-      <Text>Cuisine</Text>
-      <Text>Type of Restaurant</Text>
-      <Text>Rating</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
+    const cuisine = [
+      { id: 1, optionText: 'Chinese' },
+      { id: 2, optionText: 'American' },
+      { id: 3, optionText: 'Mexican' },
+      { id: 4, optionText: 'Italian' },
+      { id: 5, optionText: 'Japanese' },
+      { id: 6, optionText: 'Korean' },
+      { id: 7, optionText: 'Thai' },
+      { id: 7, optionText: 'Vietnamese' },
+      { id: 7, optionText: 'Indian' }
+    ];
 
-export default PreferencesScreen;
+    const restaurant = [
+      { id: 1, optionText: 'Breakfast' },
+      { id: 2, optionText: 'Brunch' },
+      { id: 3, optionText: 'Bar' },
+      { id: 4, optionText: 'Fast Food' },
+      { id: 5, optionText: 'Dessert' },
+      { id: 6, optionText: 'Drink' },
+      { id: 7, optionText: 'Coffee Shop' },
+      { id: 7, optionText: 'BBQ' },
+      { id: 7, optionText: 'Dinner' }
+    ];
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '80%', margin: 'auto' }}>
+        <Text style={styles.centerHeader}>Where would you like to eat?*</Text>
+        <TextInput
+          placeholder="Enter zipcode"
+          style={styles.inputBox}
+        />
+        <Text style={styles.centerHeader}>When would you like to eat?*</Text>
+        <TextInput
+          placeholder="Enter time"
+          style={styles.inputBox}
+        />
+        <Text style={styles.header}>Budget</Text>
+        <SelectionGroup 
+          renderContent={this.renderButton}
+          items={budgetData}
+          onPress={this.budgetSelectionHandler.selectionHandler}
+          isSelected={this.budgetSelectionHandler.isSelected}
+          containerStyle={styles.answers}
+          onItemSelected={(item) => this.setState({ selectedAnswer: item })}
+          onItemDeselected={(item, selectedItems) => this.setState({selectedAnswer: null})}
+        />
+        <Text style={styles.header}>Dietary Requirements</Text>
+        <SelectionGroup 
+          renderContent={this.renderButton}
+          items={dietaryRequirements}
+          onPress={this.dietSelectionHandler.selectionHandler}
+          isSelected={this.dietSelectionHandler.isSelected}
+          containerStyle={styles.answers}
+          onItemSelected={(item) => this.setState({ selectedAnswer: item })}
+          onItemDeselected={(item, selectedItems) => this.setState({selectedAnswer: null})}
+        />
+        <Text style={styles.header}>Cuisine</Text>
+        <SelectionGroup 
+          renderContent={this.renderButton}
+          items={cuisine}
+          onPress={this.cuisineSelectionHandler.selectionHandler}
+          isSelected={this.cuisineSelectionHandler.isSelected}
+          containerStyle={styles.answers}
+          onItemSelected={(item) => this.setState({ selectedAnswer: item })}
+          onItemDeselected={(item, selectedItems) => this.setState({selectedAnswer: null})}
+        />
+        <Text style={styles.header}>Type of Restaurant</Text>
+        <SelectionGroup 
+          renderContent={this.renderButton}
+          items={restaurant}
+          onPress={this.restaurantSelectionHandler.selectionHandler}
+          isSelected={this.restaurantSelectionHandler.isSelected}
+          containerStyle={styles.answers}
+          onItemSelected={(item) => this.setState({ selectedAnswer: item })}
+          onItemDeselected={(item, selectedItems) => this.setState({selectedAnswer: null})}
+        />
+        <Text style={styles.header}>Rating</Text>
+        <Button title="Go back" onPress={() => this.props.navigation.goBack()} />
+      </View>
+    );
+  } 
+};
+
+const styles = StyleSheet.create({
+  inputBox: {
+    backgroundColor: '#FFF',
+    width: '80%',
+    height: 36,
+    paddingLeft: 5,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  button: {
+      padding: 5,
+      marginTop: 5,
+      marginBottom: 5,
+      height: 36,
+      width: '30%',
+      borderRadius: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  buttonText: {
+      textAlign: 'center',
+      fontSize: 12
+  },
+  answers: {
+      width: '80%',
+      alignSelf: 'center',
+      justifyContent: 'space-between',
+      marginBottom: 10,
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+  },
+  header: {
+    width: '80%',
+    textAlign: 'left',
+    color: '#6B222D',
+    fontSize: 14
+  },
+  centerHeader: {
+    width: '80%',
+    textAlign: 'center',
+    color: '#6B222D',
+    fontSize: 14
+  }
+});
