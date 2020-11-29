d4523074d89de72ad804212413ac170965f26547
Jessica Nguyen <jdn039@ucsd.edu>
2020-11-29 16:31:24 -0600

    room code generator

--- App.js
+++ App.js
@@ -38,12 +38,21 @@ class App extends Component {
 
     this.state = {
       isDarkmode: true,
-      groupCode: "ABCD"
+      groupCode: this.roomCodeGenerator(4)
     };
     var mode = (this.getIsDarkmode() ? styles.darkmode : styles.lightmode);
     console.log(mode);
-
   }
+
+  roomCodeGenerator = (length) => {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
   toggleDarkmode() {
     this.setState({ isDarkmode: !this.state.isDarkmode});
